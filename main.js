5eb628bd34284e27fd749ff444e18546c20200bc
Lucas M. Mororó <93172335+Lucas-Mororo@users.noreply.github.com>
2021-10-27 09:21:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+  github: 'Lucas-Mororo',
+  youtube: '',
+  facebook: 'profile.php?id=100004501296743',
+  instagram: 'lucasmmartinss_',
+  twitter: 'Lcmororo_'
+}
+
+// function changeSocialMediaLinks()
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
